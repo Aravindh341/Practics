2f6f8c9de8d15947ec4456b01874d3482187b72b
Aravindh341 <126602450+Aravindh341@users.noreply.github.com>
2023-06-30 20:01:36 +0530

    Files

---
+++ app/controllers/practics_controller.rb
@@ -0,0 +1,6 @@
+class PracticsController < ApplicationController
+    def index
+    end
+    def about
+    end
+end
---
+++ test/controllers/practics_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PracticsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
